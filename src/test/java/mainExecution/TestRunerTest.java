3f5d9ed8b32ac20f5dd484f6b93edba896118114
SPANADC\Amit.Kumar <amit.kumar@spanservices.com>
2017-12-14 13:12:51 +0530

    commit

--- src/test/java/mainExecution/TestRunerTest.java
+++ src/test/java/mainExecution/TestRunerTest.java
@@ -12,14 +12,14 @@ import com.cucumber.listener.Reporter;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/feature/1 (3).feature", glue = { "stepDefinition" }, plugin = {
-		"json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html" })
+		"json:target/cucumber.json", })//"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"
 public class TestRunerTest {
 
-	@AfterClass
+/*	@AfterClass
 	public static void teardown() {
-		Reporter.loadXMLConfig(new File("src/test/java/extent-config.xml"));
+		Reporter.loadXMLConfig(new File("src/test/resource/extent-config.xml"));
 		Reporter.setSystemInfo("user", System.getProperty("user.name"));
 		Reporter.setSystemInfo("os", "Win 8");
 		Reporter.setTestRunnerOutput("Sample test runner output message");
-	}
+	}*/
 }
